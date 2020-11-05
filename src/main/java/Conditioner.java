c9a01bec75c3a274a2402250294af014bc5cc8d1
Oleg Matveenko <om1190@yandex.ru>
2020-11-05 13:38:56 +0500

    conditioner test

---
+++ src/main/java/Conditioner.java
@@ -0,0 +1,55 @@
+public class Conditioner {
+    private int maxTemperature = 25;
+    private int minTemperature = 15;
+    private int currentTemperature;
+
+    public int getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public int getMinTemperature() {
+        return minTemperature;
+    }
+
+    public int getCurrentTemperature() {
+        return currentTemperature;
+    }
+
+    public void setCurrentTemperature(int currentTemperature) {
+//        проверка, что текущ t меньше max
+        if (currentTemperature > maxTemperature)
+//        если текущ t больше max, то return приведет к выходу
+//        и вернет последнее присвоенное знач, то есть 0
+            return;
+//        проверка что текущ t больше min
+        if (currentTemperature < minTemperature)
+//            если текущ t меньше min, то return приведет к выходу
+//        и вернет последнее присвоенное знач, то есть 0
+            return;
+//        если проверка проходит, то есть меньше max и больше min, то
+//        в поле currentTemperature текущего объекта записывается
+//        текущее значение переменной/параметра, то есть то, кот
+//        было передано в сеттер
+        this.currentTemperature = currentTemperature;
+    }
+
+    public void increaseCurrentTemperature() {
+        if (currentTemperature < maxTemperature)
+            currentTemperature++;
+    }
+
+    public void decreaseCurrentTemperature() {
+
+        if (currentTemperature > minTemperature)
+            currentTemperature--;
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/ConditionerTest.java
@@ -0,0 +1,102 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ConditionerTest {
+
+    @Test
+    public void shouldSetCurrentTemperatureBetweenMinMax() {
+        Conditioner conditioner = new Conditioner();
+
+//        проверяем, что текущая t инициализируется нулем
+        assertEquals(0, conditioner.getCurrentTemperature());
+//        устанавливаем текущую температуру
+        conditioner.setCurrentTemperature(20);
+//        проверям, что текущая t установилась 20
+        assertEquals(20, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    public void shouldSetCurrentTemperatureOverMax() {
+        Conditioner conditioner = new Conditioner();
+
+//        проверяем, что текущая t инициализируется нулем
+        assertEquals(0, conditioner.getCurrentTemperature());
+//        устанавливаем текущую температуру, больше max
+        conditioner.setCurrentTemperature(26);
+//        проверям, что не допустимое значение не принято и текущая t
+//        приняла инициализированное нулевое значение
+        assertEquals(0, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    public void shouldSetCurrentTemperatureUnderMin() {
+        Conditioner conditioner = new Conditioner();
+
+        assertEquals(0, conditioner.getCurrentTemperature());
+        conditioner.setCurrentTemperature(14);
+        assertEquals(0, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    public void shouldSetCurrentTemperatureMax() {
+        Conditioner conditioner = new Conditioner();
+
+        assertEquals(0, conditioner.getCurrentTemperature());
+        conditioner.setCurrentTemperature(25);
+        assertEquals(25, conditioner.getMaxTemperature());
+    }
+
+    @Test
+    public void shouldSetCurrentTemperatureMin() {
+        Conditioner conditioner = new Conditioner();
+
+        assertEquals(0, conditioner.getCurrentTemperature());
+        conditioner.setCurrentTemperature(15);
+        assertEquals(15, conditioner.getMinTemperature());
+    }
+
+    @Test
+    public void shouldIncreaseCurrentTemperatureBetweenMinMax() {
+        Conditioner conditioner = new Conditioner();
+
+        conditioner.setCurrentTemperature(20);
+        assertEquals(20, conditioner.getCurrentTemperature());
+
+        conditioner.increaseCurrentTemperature();
+        assertEquals(21, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    public void shouldIncreaseCurrentTemperatureMax() {
+        Conditioner conditioner = new Conditioner();
+
+        conditioner.setCurrentTemperature(26);
+        assertEquals(25, conditioner.getCurrentTemperature());
+
+        conditioner.increaseCurrentTemperature();
+        assertEquals(25, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    public void shouldDecreaseCurrentTemperatureBetweenMinMax() {
+        Conditioner conditioner = new Conditioner();
+
+        conditioner.setCurrentTemperature(18);
+        assertEquals(18, conditioner.getCurrentTemperature());
+
+        conditioner.decreaseCurrentTemperature();
+        assertEquals(17, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    public void shouldDecreaseCurrentTemperatureMin() {
+        Conditioner conditioner = new Conditioner();
+
+        conditioner.setCurrentTemperature(16);
+        assertEquals(15, conditioner.getCurrentTemperature());
+
+        conditioner.decreaseCurrentTemperature();
+        assertEquals(15, conditioner.getCurrentTemperature());
+    }
+}
